28853750cfe319da307297bc1bb6063624abb556
Ussrfantom <dimkakrohin@mail.ru>
2021-09-15 23:12:02 +0300

    add firestore

--- app/src/main/java/ussrfantom/com/example/animetest/Screens/ChatScreen/ChatActivity.java
+++ app/src/main/java/ussrfantom/com/example/animetest/Screens/ChatScreen/ChatActivity.java
@@ -1,5 +1,6 @@
 package ussrfantom.com.example.animetest.Screens.ChatScreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,6 +9,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +25,13 @@ import ussrfantom.com.example.animetest.pojo.Message;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private FirebaseFirestore db;
+
     private RecyclerView recyclerViewMessages;
     private MessagesAdapter adapter;
     private EditText editTextMessage;
     private ImageView imageViewSendMessage;
 
-    private List<Message> messages;
     private String author;
 
 
@@ -31,13 +39,14 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        db = FirebaseFirestore.getInstance();
         recyclerViewMessages = findViewById(R.id.recyclerViewMessage);
         editTextMessage =findViewById(R.id.editTextMessage);
         imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
         adapter = new MessagesAdapter();
         recyclerViewMessages.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewMessages.setAdapter(adapter);
-        messages = new ArrayList<>();
+
         author = "Дмитрий";
         imageViewSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,10 +59,18 @@ public class ChatActivity extends AppCompatActivity {
   private void sendMessage(){
         String textOfMessage = editTextMessage.getText().toString().trim();
         if (!textOfMessage.isEmpty()){
-            messages.add(new Message(author, textOfMessage));
-            adapter.setMessages(messages);
-            editTextMessage.setText("");
             recyclerViewMessages.scrollToPosition(adapter.getItemCount() -1);
+            db.collection("messages").add(new Message(author, textOfMessage)).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    editTextMessage.setText("");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull  Exception e) {
+                    Toast.makeText(ChatActivity.this, "Сообщение не отправлено", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
   }
 
